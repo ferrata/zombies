4a940b4ca5e20996f45f45a3fc9021dd461c889d
Sasha Goloshchapov <ferrata@hey.com>
2023-04-29 15:22:38 -0500

    feat: bullets animation

---
+++ src/objects/CasingEmitter.ts
@@ -0,0 +1,124 @@
+import GameScene from "../scenes/GameScene";
+
+export class CasingEmitter extends Phaser.GameObjects.Container {
+  private emitter: Phaser.GameObjects.Particles.ParticleEmitter;
+  private emitterAngle: number;
+  private emitterOfffset: { x: number; y: number };
+  private isDark: boolean;
+
+  constructor(
+    scene: GameScene,
+    x: number,
+    y: number,
+    casingsTexture: string,
+    casingsScale: { from: number; to: number }
+  ) {
+    super(scene, x, y);
+
+    const textures = this.scene.textures.get(casingsTexture);
+    this.emitter = this.scene.add.particles(0, 0, textures, {
+      angle: (): number => {
+        return this.emitterAngle + 90 + Phaser.Math.Between(-15, 15);
+      },
+      rotate: (): number => {
+        return this.emitterAngle + 90 + Phaser.Math.Between(-25, 25);
+      },
+      maxVelocityX: {
+        onEmit: (): number => {
+          return Phaser.Math.Between(220, 245);
+        },
+        onUpdate: (particle: Phaser.GameObjects.Particles.Particle): number => {
+          if (particle.lifeT > 0.5) {
+            return 0;
+          } else if (particle.lifeT > 0.3) {
+            return Phaser.Math.Between(5, 10);
+          } else if (particle.lifeT > 0.1) {
+            return 10;
+          }
+
+          return particle.maxVelocityX;
+        },
+      },
+      maxVelocityY: {
+        onUpdate: (particle: Phaser.GameObjects.Particles.Particle): number => {
+          if (particle.lifeT > 0.5) {
+            return 0;
+          } else if (particle.lifeT > 0.3) {
+            return Phaser.Math.Between(5, 10);
+          } else if (particle.lifeT > 0.1) {
+            return 10;
+          }
+
+          return Phaser.Math.Between(220, 245);
+        },
+      },
+      lifespan: { min: 900, max: 2000 },
+      speed: { min: 20, max: 245 },
+      scale: {
+        onEmit: () => {
+          return casingsScale.from;
+        },
+        onUpdate: (particle: Phaser.GameObjects.Particles.Particle): number => {
+          if (particle.lifeT < 0.08) {
+            return particle.scaleX;
+          }
+
+          return casingsScale.to;
+        },
+      },
+      emitting: false,
+      stopAfter: 1,
+    });
+
+    this.emitter.onParticleEmit(
+      (particle: Phaser.GameObjects.Particles.Particle) => {
+        particle.setPosition(this.emitterOfffset.x, this.emitterOfffset.y);
+        if (this.isDark) {
+          particle.tint = 0x000000;
+        }
+      }
+    );
+
+    this.scene.add.existing(this.emitter);
+  }
+
+  public setEmitterPosition(
+    center: Phaser.Math.Vector2,
+    length: number,
+    angle: number
+  ) {
+    const offset = new Phaser.Math.Vector2(center)
+      .setLength(length)
+      .rotate(Phaser.Math.DegToRad(angle));
+
+    this.emitterOfffset = { x: center.x + offset.x, y: center.y + offset.y };
+  }
+
+  public setEmitterAngle(angle: number) {
+    this.emitterAngle = angle;
+  }
+
+  public onLight() {
+    this.isDark = false;
+    this.updateTintForAllAlive(0xffffff);
+  }
+
+  public onDark() {
+    this.isDark = true;
+    this.updateTintForAllAlive(0x000000);
+  }
+
+  public emitOne(casingName: string) {
+    this.emitter.setEmitterFrame(casingName);
+    this.emitter.start();
+  }
+
+  private updateTintForAllAlive(tint: number) {
+    this.emitter.forEachAlive(
+      (particle: Phaser.GameObjects.Particles.Particle) => {
+        particle.tint = tint;
+      },
+      this.emitter
+    );
+  }
+}
