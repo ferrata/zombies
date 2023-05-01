fde0543a7ad8141364ae9f0781a1959df31cc3e2
Sasha Goloshchapov <ferrata@hey.com>
2023-05-01 11:52:16 -0500

    feat: added weapon mode

--- src/scenes/Event.ts
+++ src/scenes/Event.ts
@@ -3,5 +3,7 @@ export enum Event {
   UNKNOWN_OBJECT = "object:unknown",
   NO_OBJECT_IN_RANGE = "object:none-in-range",
   OBJECT_PICKED_UP = "object:picked-up",
-  INTERACT_WITH_OBJECT = "interact",
+  INTERACT_WITH_OBJECT = "object:interact",
+
+  WEAPON_MODE_CHANGED = "weapon:mode-changed",
 }
